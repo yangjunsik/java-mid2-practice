47be6e0270b0599d0c61289375520ac2250b5d5b
yangjunsig <wnstlr1379@naver.com>
2026-04-27 22:42:01 +0900

    ArrayList 직접구현 기본

---
+++ src/array/arraylist/MyArrayListV1.java
@@ -0,0 +1,52 @@
+package array.arraylist;
+
+import java.util.Arrays;
+
+public class MyArrayListV1 {
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV1() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV1(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(Object e) {
+        elementData[size] = e;
+        size++;
+    }
+
+    public Object get(int index) {
+        return elementData[index];
+    }
+
+    public Object set(int index, Object e) {
+        Object old = elementData[index];
+        elementData[index] = e;
+        return old;
+    }
+
+    public int indexOf(Object e) {
+        for (int i = 0; i < size; i++) {
+            if (elementData[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) + " size = " + size +
+                ", capacity = " + elementData.length;
+    }
+}
