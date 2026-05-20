8456278513e9085a03a94b7c2aa20cee25581ac7
yangjunsig <wnstlr1379@naver.com>
2026-05-20 17:17:21 +0900

    해쉬셋 Object 활용

---
+++ src/collection/set/MyHashSetV2.java
@@ -0,0 +1,76 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV2 {
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    LinkedList<Object>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV2() {
+        initBuckets();
+    }
+
+    public MyHashSetV2(int initialCapacity) {
+        this.capacity = initialCapacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(Object value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) {
+            return false;
+        }
+        bucket.add(value);
+        size++;
+        return true;
+    }
+
+    public boolean contains(Object searchValue) {
+        int hashIndex = hashIndex(searchValue);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        return bucket.contains(searchValue);
+    }
+
+    public boolean remove(int searchValue) {
+        int hashIndex = hashIndex(searchValue);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        boolean result = bucket.remove(Integer.valueOf(searchValue));
+        if (result) {
+            size--;
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    private int hashIndex(Object value) {
+        return Math.abs(value.hashCode()) % capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV2{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+
+}
