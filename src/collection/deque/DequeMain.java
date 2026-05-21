3d4aff0d92ed7165908d64e6c53d93f410e83b6d
yangjunsig <wnstlr1379@naver.com>
2026-05-21 15:32:16 +0900

    Deque 기본

---
+++ src/collection/deque/DequeMain.java
@@ -0,0 +1,30 @@
+package collection.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeMain {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        //Deque<Integer> deque = new LinkedList<>();
+
+        //데이터 추가
+        deque.offerFirst(1);
+        System.out.println(deque);
+        deque.offerFirst(2);
+        System.out.println(deque);
+        deque.offerLast(3);
+        System.out.println(deque);
+        deque.offerLast(4);
+        System.out.println(deque);
+
+        //다음에 꺼낼 데이터 확인(꺼내지 않고 단순 조회만)
+        System.out.println(deque.peekFirst());
+        System.out.println(deque.peekLast());
+
+        //데이터 꺼내기
+        System.out.println(deque.pollFirst());
+        System.out.println(deque.pollLast());
+        System.out.println(deque);
+    }
+}
